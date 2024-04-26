8a7b73e6200e69e63b0ec0b63ac02f7769d44868
yongbin <1103782715@qq.com>
2024-04-26 14:29:45 +0800

    修改登陆引导，添加采购助理页面

--- models/user2_collects.go
+++ models/user2_collects.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 // Auth2Collects 记录用户什么时候收藏了什么文章
-type Auth2Collects struct {
+type User2Collects struct {
 	UserID       uint         `gorm:"primaryKey"`
 	AuthModel    ArticleModel `gorm:"foreignKey:UserID"`
 	ArticleID    uint         `gorm:"primaryKey"`
